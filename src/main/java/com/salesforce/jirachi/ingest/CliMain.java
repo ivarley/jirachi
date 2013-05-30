98d1f1a9d7cd00a4473603b0b9000bcb65afb8e2
Ian Varley <ivarley@salesforce.com>
2013-05-30 12:49:38 -0500

    Empty java project and maven pom

---
+++ src/main/java/com/salesforce/jirachi/ingest/CliMain.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright, 1999-2012, SALESFORCE.com 
+ * All Rights Reserved
+ * Company Confidential
+ */
+package com.salesforce.jirachi.ingest;
+
+/**
+ * Runs the jirachi import process
+ */
+public class CliMain {
+    public static void main(String[] args) {
+	System.out.println("Hello world.");
+    }
+}
---
+++ src/test/java/com/salesforce/jirachi/cli/CliMainTest.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright, 1999-2012, SALESFORCE.com
+ * All Rights Reserved
+ * Company Confidential
+ */
+package com.salesforce.jirachi.cli;
+
+import org.junit.Test;
+
+import com.salesforce.jirachi.ingest.CliMain;
+
+/**
+ * CliMainTest
+ */
+public class CliMainTest {
+
+    @Test
+    public void run() {
+	 CliMain.main(null);
+    }
+
+
+}
